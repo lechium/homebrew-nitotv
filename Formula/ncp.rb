f5405b862490e6df642f205f48edec16d9d107d0
lechium <kevin.w.bradley@me.com>
2022-08-28 18:49:19 -0700

    minor fix

--- Formula/ncp.rb
+++ Formula/ncp.rb
@@ -2,8 +2,7 @@ class Ncp < Formula
   desc "Safe replacement for 'cp' with progress indication."
   homepage "https://github.com/lechium/ncp"
   url "https://github.com/lechium/ncp.git",
-    tag:      "1.0",
-    revision: "ef5f0ed3829cb3bed527af6d812d5fbd1642a136"
+    revision: "0a495fe2954da4161bd8679a2bb2f725a0c5abb9"
   license "MIT"
   head "https://github.com/lechium/ncp.git",
     branch: "master"
